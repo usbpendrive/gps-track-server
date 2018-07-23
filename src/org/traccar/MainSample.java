c7c0551518fbc7e20097a7ed57e4500202540973
usbpendrive <ferry.fernando@gmail.com>
2018-07-23 14:56:42 +0700

    Main Sample App

--- src/org/traccar/MainSample.java
+++ src/org/traccar/MainSample.java
@@ -1,7 +1,18 @@
 package org.traccar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainSample {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String name = reader.readLine();
+        } catch (IOException ex) {
+            //
+        }
     }
 }
